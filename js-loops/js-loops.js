37b0abe6d1db8db96d0dfe4f914a1ac1b4ec16e4
davideguerrero <davidencaladaguerrero@gmail.com>
2019-05-27 19:44:39 +0200

    loops

--- js-loops/js-loops.js
+++ js-loops/js-loops.js
@@ -1,69 +1,85 @@
 console.log("-- init script");
 
 var myTitle;
-// a variable is undefined because it has no value assigned to it
+/* the variable is undefined because it has no value assigned to it */
+console.log(myTitle);
+myTitle = document.getElementById('name');
+/* the variable returns as null because it has a wrong value assigned to it */
+console.log(myTitle);
+myTitle = document.getElementById('appTitle').innerText;
+/* the value of the variable gets logged */
 console.log(myTitle);
 
-myTitle = document.getElementById("name");
-// a variable returns as null because it has a wrong value assigned to it
-console.log(myTitle)
 
-myTitle = document.getElementById("appTitle").innerHTML;
-// the value of the variable gets log
-console.log(myTitle)
+/* Say my Name APP */
 
-// Say my name APP
+var allTheNames = [];
+var nameButton = document.getElementById('nameButton');
+var nameInput = document.getElementById('myName');
+var messageBox = document.getElementById('messageBox');
+var undoButton = document.getElementById('undoButton');
+var allButton = document.getElementById('allButton');
 
-// DEFINE A VARIABLE
-var allNames = [];
-var nameButton = document.getElementById("nameButton");
-var nameInput = document.getElementById("myName");
-var messageBox = document.getElementById("messageBox");
+nameButton.addEventListener('click', sayMyName);
+undoButton.addEventListener('click', deleteName);
+allButton.addEventListener('click', showName);
 
-nameButton.addEventListener("click", sayMyName);
+// document.getElementById('listNames').innerHTML = allTheNames;
+
+function deleteName() {
+  updateTheMessage('Successfuly deleted', true);
+
+  allTheNames.pop();
+  document.getElementById('listNames').innerHTML = allTheNames;
+}
+
+// function deleteName() {
+//   allTheNames.pop();
+//   document.getElementById('listNames').innerHTML = allTheNames;
+// }
+
+function showName() {
+  document.getElementById('listNames').innerHTML = allTheNames;
+}
 
 function sayMyName() {
 
-  // console.log(nameInput.value.lenght);
-
-  if(nameInput.value.length > 0) {
-    allNames.push(nameInput.value);
-
-    // empty the imput
+  if ( nameInput.value.length > 0 ) {
+    // if there are more than 0 characters in the input field
+    allTheNames.push(nameInput.value);
+    // empty the input
     nameInput.value = "";
     // put the cursor in the input field
     nameInput.focus();
 
+    // pick the last name from the array
+    var lastNameAdded = allTheNames[allTheNames.length - 1];
     // SUCCESS
-    var lastNameAdded = allNames[allNames.length -1];
-    updateTheMessage("You have successfuly added" + + "to the list!", true);
-    
-    console.log(allNames);
+    updateTheMessage(
+      "You have successfuly added " + lastNameAdded + " to the list", true);
   } else {
     // ERROR
-    updateTheMessage("No name entered!", false);
-    
-    console.log("No name");
+    updateTheMessage("No name entered", false);
   }
 
-  // check if it's not empty
-
-  // empty the imput and focus on it
-
-  // console.log("Button clicked");
-
-  // STORE IN THAT VARIABLE USING THE .push("value") METHOD
+  console.log(allTheNames);
 }
 
 function updateTheMessage(messageText, messageSuccess) {
   messageBox.innerText = messageText;
 
   if (messageSuccess == true) {
-    messageBox.classList.add("message-success");
-    messageBox.classList.remove("message-error");
+    messageBox.classList.add('message-success');
+    messageBox.classList.remove('message-error');
   } else {
-    messageBox.classList.add("message-error");
-    messageBox.classList.remove("message-success");
+    messageBox.classList.add('message-error');
+    messageBox.classList.remove('message-success');
   }
-  
+
 }
+
+/*  
+  - The undo button should remove last name added
+  - Give you a message about it
+  - "List all the names" button should print all the names in an unordered list
+*/
