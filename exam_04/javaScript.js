1a86ee322aa8cd48f47cc99d9db1d53319687940
davideguerrero <davidencaladaguerrero@gmail.com>
2019-07-18 18:09:39 +0200

    fixed

--- exam_04/javaScript.js
+++ exam_04/javaScript.js
@@ -1,7 +1,5 @@
 var showMenu = document.getElementById('openMenu');
 var hideMenu = document.getElementById('closeMenu');
-var showGrid = document.getElementById('displayGrid');
-var showList = document.getElementById('displayList');
 
 showMenu.addEventListener('click', showOpenMenu );
 
@@ -13,4 +11,15 @@ hideMenu.addEventListener('click', hideOpenMenu );
 
 function hideOpenMenu() {
   TweenMax.to('#nav', 1, {y: '-100%'});
+}
+
+function showListView() {
+  var showList = document.getElementById('photoGrid');
+  showList.classList.remove('photoGrid');
+  showList.classList.add('gridPhoto');
+}
+
+function showGridView() {
+  var showGrid = document.getElementById('photoGrid');
+  showGrid.classList.add('photoGrid');
 }
